df847c6a6947a89088dda03dc0010c1adebbfb3e
kunyoung709 <kunyoung709@naver.com>
2024-05-29 12:51:30 +0900

    App.tsx에 PageFormat 추가.

--- issue_manager/src/App.tsx
+++ issue_manager/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import SignupForm from './components/SignupForm/SignupForm';
+//import SignupForm from './components/SignupForm/SignupForm';
 // import LoginForm from './components/LoginForm/LoginForm';
+import PageFormat from './components/PageFormat/PageFormat'
 
 const App: React.FC = () => {
   return (
     <div>
-      <SignupForm></SignupForm>
+      {/* <SignupForm></SignupForm> */}
       {/* <LoginForm></LoginForm> */}
+      <PageFormat></PageFormat>
     </div>
   );
 }
