9c4fc14c712e4442b4855e6ba789020ee6f37664
kunyoung709 <kunyoung709@naver.com>
2024-05-29 14:35:32 +0900

    side menu 버튼 동작 추가

--- issue_manager/src/components/PageFormat/PageFormat.tsx
+++ issue_manager/src/components/PageFormat/PageFormat.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './PageFormat.css';
 
+
 import { VscGraph } from "react-icons/vsc";
 import { IoMdNotificationsOutline } from "react-icons/io";
 import { MdOutlineStars } from "react-icons/md";
@@ -15,86 +16,105 @@ import { CiMap } from "react-icons/ci";
 
 const PageFormat = () => {
     const [isSdaExpanded, setIsSdaExpanded] = useState(false);
+    const [currentComponent, setCurrentComponent] = useState('A');
 
     const toggleSdaArrow = () => {
         setIsSdaExpanded(!isSdaExpanded);
     };
 
+    const renderComponent = () => {
+        switch (currentComponent) {
+            case 'A':
+                return <span>New Issue</span>;
+            case 'B':
+                return <span>Search</span>;
+            case 'C':
+                return <span>Inbox</span>;
+            case 'D':
+                return <span>My Issue</span>;
+            default:
+                return <span>New Issue</span>;
+        }
+    }
+
     return (
-            <div className='page'>
-                <div className='sideMenu'>
-                    <button>
-                        <div className='buttonUsername'>
-                            <FaRegUser />
-                            <span id='userName'>User Name</span>
-                            <IoIosArrowForward />
-                        </div>
-                    </button>
-                    <button>
-                        <div className='buttonContent'>
-                            <PiListPlusFill />
-                            <span className='buttonLabel'>New Issue</span>
-                        </div>
-                    </button>
-                    <button>
-                        <div className='buttonContent'>
-                            <GoSearch />
-                            <span className='buttonLabel'>Search</span>
-                        </div>
-                    </button>
-                    <button>
-                        <div className='buttonContent'>
-                            <RiInbox2Fill />
-                            <span className='buttonLabel'>Inbox</span>
-                        </div>
-                    </button>
-                    <button>
-                        <div className='buttonContent'>
-                            <VscIssues />
-                            <span className='buttonLabel'>My Issue</span>
-                        </div>
-                    </button>
-                    <button>
-                        <div className='buttonContent'>
-                            <CiMap />
-                            <span className='buttonLabel'>Road Map...</span>
-                        </div>
-                    </button>
-                    <p id='yourTeam'>Your Team</p>
-                    <button onClick={toggleSdaArrow}>
-                        <div className='buttonContent'>
-                            <span id='arrow'>{isSdaExpanded ? <BiSolidDownArrow /> : <BiSolidUpArrow />}</span>
-                            <span className='buttonLabel'>SDA</span>
-                        </div>
-                    </button>
-                    <button>
-                        <div className='buttonContent'>
-                            <VscGraph />
-                            <span className='buttonLabel'>Statistics</span>
-                        </div>
-                    </button>
-                    <button>
-                        <div className='buttonContent'>
-                            <MdOutlineStars />
-                            <span className='buttonLabel'>Ranking</span>
-                        </div>
-                    </button>
-                    <button>
-                        <div className='buttonContent'>
-                            <IoMdNotificationsOutline />
-                            <span className='buttonLabel'>Notify</span>
-                        </div>
-                    </button>
-                </div>
-                <div className='topBanner'>
-                    <div id='menuName'>
-                        menu name
+        <div className='page'>
+            <div className='sideMenu'>
+                <button>
+                    <div className='buttonUsernameContent'>
+                        <FaRegUser />
+                        <span id='userName'>User Name</span>
+                        <IoIosArrowForward />
                     </div>
+                </button>
+                <button onClick={() => setCurrentComponent('A')}>
+                    <div className='buttonContent'>
+                        <PiListPlusFill />
+                        <span className='buttonLabel'>New Issue</span>
+                    </div>
+                </button>
+                <button onClick={() => setCurrentComponent('B')}>
+                    <div className='buttonContent'>
+                        <GoSearch />
+                        <span className='buttonLabel'>Search</span>
+                    </div>
+                </button>
+                <button onClick={() => setCurrentComponent('C')}>
+                    <div className='buttonContent'>
+                        <RiInbox2Fill />
+                        <span className='buttonLabel'>Inbox</span>
+                    </div>
+                </button>
+                <button onClick={() => setCurrentComponent('D')}>
+                    <div className='buttonContent'>
+                        <VscIssues />
+                        <span className='buttonLabel'>My Issue</span>
+                    </div>
+                </button>
+                <button>
+                    <div className='buttonContent'>
+                        <CiMap />
+                        <span className='buttonLabel'>Road Map...</span>
+                    </div>
+                </button>
+                <p id='yourTeam'>Your Team</p>
+                <button onClick={toggleSdaArrow}>
+                    <div className='buttonContent'>
+                        <span id='arrow'>{isSdaExpanded ? <BiSolidDownArrow /> : <BiSolidUpArrow />}</span>
+                        <span className='buttonLabel'>SDA</span>
+                    </div>
+                </button>
+                <button>
+                    <div className='buttonContent'>
+                        <VscGraph />
+                        <span className='buttonLabel'>Statistics</span>
+                    </div>
+                </button>
+                <button>
+                    <div className='buttonContent'>
+                        <MdOutlineStars />
+                        <span className='buttonLabel'>Ranking</span>
+                    </div>
+                </button>
+                <button>
+                    <div className='buttonContent'>
+                        <IoMdNotificationsOutline />
+                        <span className='buttonLabel'>Notify</span>
+                    </div>
+                </button>
+            </div>
+            <div className='topBanner'>
+                <div id='menuName'>
+                    menu name
                 </div>
-                <div className='main'>
-                    <div> main page content </div>
+                <div>
+                    additional buttons
                 </div>
             </div>
+            <div className='main'>
+                {renderComponent()}
+            </div>
+        </div>
     );
 };
 
