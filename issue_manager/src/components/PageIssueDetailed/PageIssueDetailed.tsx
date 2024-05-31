53147adc04cfedcd6b0e1aba98fc738be513ec4f
kunyoung709 <kunyoung709@naver.com>
2024-06-01 02:41:13 +0900

    검색 페이지의 검색 기능, 이슈 상세 페이지의 댓글목록 보기 기능 구현 완료, 테스트용 함수 추가. 댓글 추가 기능은 추가해야됨.

--- issue_manager/src/components/PageIssueDetailed/PageIssueDetailed.tsx
+++ issue_manager/src/components/PageIssueDetailed/PageIssueDetailed.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './PageIssueDetailed.css'
 import { Issue } from '../ElementIssueList/ElementIssueList';
 import DisplayCommentList from '../DisplayCommentList/DisplayCommentList.tsx'
+import { Comment } from '../ElementCommentList/ElementCommentList';
 
 import { IoIosArrowBack } from "react-icons/io";
+import { BiSolidUpArrow, BiSolidDownArrow } from "react-icons/bi";
 
 type PageIssueDetailedProps = {
     issue: Issue;
@@ -11,6 +13,51 @@ type PageIssueDetailedProps = {
 }
 
 const PageIssueDetailed: React.FC<PageIssueDetailedProps> = ({ issue, onBack }) => {
+    const [showComments, setShowComments] = useState<boolean>(false);
+    const [comments, setComments] = useState<Comment[]>([]);
+
+    // 실제 서버 연결
+    // const toggleComments = () => {
+    //     if (!showComments) {
+    //         fetch(`http://localhost:8080/issue/${issue.issueNum}/comments`)
+    //             .then(response => response.json())
+    //             .then(data => {
+    //                 if (data.success) {
+    //                     setComments(data.comments as Comment[]);
+    //                 } else {
+    //                     setComments([]);
+    //                 }
+    //             })
+    //             .catch(error => console.error('Error fetching comments:', error));
+    //     }
+    //     setShowComments(!showComments);
+    // };
+
+    // 서버 연결 없이 테스트용
+    const toggleComments = () => {
+        if (!showComments) {
+            const testComments = [
+                {
+                    issueNum: issue.issueNum,
+                    content: "테스트 댓글 1",
+                    date: "2024-05-30T11:51:47.825+00:00",
+                    commentId: 1,
+                    accountId: "testUser1"
+                },
+                {
+                    issueNum: issue.issueNum,
+                    content: "테스트 댓글 2",
+                    date: "2024-05-30T11:57:27.945+00:00",
+                    commentId: 2,
+                    accountId: "testUser2"
+                }
+            ];
+
+            setComments(testComments);
+        }
+        setShowComments(!showComments);
+    };
+
     return (
         <div>
             <div className='topBanner'>
@@ -31,17 +78,21 @@ const PageIssueDetailed: React.FC<PageIssueDetailedProps> = ({ issue, onBack }) 
                     <div className='divider'></div>
                     <div>Activity</div>
                     <div>State 변경: </div>
-                    <div>Comment 입력창: </div>
                     <div className='commentBox'>
                         <input
                             type='text'
                             placeholder='Add Comment...'
                             name='comment' />
                     </div>
-                    <div>Comment 불러오기: </div>
-                    <div className='containerComment'>
-                       <DisplayCommentList /> 
-                    </div>
+                    <button className='toggleCommentButton' onClick={toggleComments}>
+                        {showComments ? <BiSolidDownArrow /> : <BiSolidUpArrow />}
+                        {showComments ? 'Hide Comments' : 'Show Comments'}
+                    </button>
+                    {showComments && (
+                        <div className='containerComment'>
+                            <DisplayCommentList comments={comments} />
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
